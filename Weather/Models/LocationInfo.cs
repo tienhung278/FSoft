4c8d17df6d2cab1c9533a24d788f28c58c12a364
Hung Pham <hung.pham@commissionfactory.com>
2021-11-26 08:01:02 +0700

    Create models

---
+++ Weather/Models/LocationInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weather.Models
+{
+    class LocationInfo
+    {
+        public Weather current { get; set; }
+    }
+}
---
+++ Weather/Models/Weather.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weather.Models
+{
+    class Weather
+    {
+        public string[] weather_descriptions { get; set; }
+        public int uv_index { get; set; }
+        public int wind_speed { get; set; }
+    }
+}
